ae7a1d87f29dcf159eaf628614729a00b3aecba7
Salma Khodaei <khodaeisalma8@gmail.com>
2026-01-17 08:34:01 -0800

    add partial class example

---
+++ PartialClasses/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartialClasses
+{
+    public partial class Product
+    {
+        public string name;
+
+        public Product()
+        {
+            name = "";
+            // can access any members defined in another partial class
+            price = 2.43;
+        }
+
+        // if no implementation is found, definition is removed from the class by compiler
+        partial void getInfo()
+        {
+            System.Console.WriteLine("product info: ");
+        }
+    }
+}
---
+++ PartialClasses/Product2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartialClasses
+{
+    public partial class Product
+    {
+        public double price;
+
+        partial void getInfo(); // private by default
+    }
+}
